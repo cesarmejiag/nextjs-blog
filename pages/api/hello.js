35258462cc67c49eb03d3ff0c231c201c34bd22e
César Mejía <iconfidence@gmail.com>
2022-03-13 14:19:36 -0600

    Remove comment

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,4 @@
 // req = HTTP incoming message, res = HTML server response
 export default function handler(req, res) {
-  console.log(req.body.email);
   res.status(200).json({ text: "Hello" });
 }
